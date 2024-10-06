52e5136278c82143cd8cbab942841a3d2d192b17
mediaios <iosmediadev@gmail.com>
2024-10-06 22:58:16 +0800

    add video yuv stride process; add rotation process

--- mi-poc/mi-video-receive.py
+++ mi-poc/mi-video-receive.py
@@ -4,19 +4,24 @@ from agora.rtc.agora_base import *
 
 
 import time
+import  os
+from observer.path_utils import get_log_path_with_filename
 from observer.mi_connection_observer import MiConnectionObserver
 from observer.mi_audio_frame_observer import MiAudioFrameObserver
 from observer.mi_local_user_observer import MiLocalUserObserver
 from observer.mi_video_frame_observer import MiVideoFrameObserver
 from agora.rtc.agora_service import AgoraServiceConfig, AgoraService
+from observer.mi_zhipu_video_frame_observer import MiZhiPuVideoFrameObserver
 
 from agora.rtc.local_user_observer import IRTCLocalUserObserver
 from agora.rtc.rtc_connection import RTCConnConfig
 config = AgoraServiceConfig()
-config.appid = "8fec0f4cc82a4689b6ee0e64d50ca33d"
+config.appid = "20338919f2ca4af4b1d7ec23d8870b56"
 config.audio_scenario = AudioScenarioType.AUDIO_SCENARIO_CHORUS
 agora_engine = AgoraService()
 config.enable_video = 1
+config.log_path = get_log_path_with_filename(os.path.splitext(__file__)[0])
+print("qidebug, log path",config.log_path)
 agora_engine.initialize(config)
 
 def create_conn(channel_id,uid=0):
@@ -51,6 +56,8 @@ def create_conn(channel_id,uid=0):
     video_track = agora_engine.create_custom_video_track_frame(video_sender)
     video_frame_observer = MiVideoFrameObserver()
     local_user.register_video_frame_observer(video_frame_observer)
+    # video_frame_observer = MiZhiPuVideoFrameObserver()
+    # local_user.register_video_frame_observer(video_frame_observer)
     video_track.set_enabled(1)
     # local_user.subscribe_all_video()
 
@@ -65,7 +72,7 @@ def create_conn(channel_id,uid=0):
     print("release")
 
 
-create_conn("fsddd",9999999)
+create_conn("qitest",9999999)
 
 agora_engine.release()
 print("QiDebug, end")
--- mi-poc/observer/mi_zhipu_video_frame_observer.py
+++ mi-poc/observer/mi_zhipu_video_frame_observer.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from agora.rtc.video_frame_observer import IVideoFrameObserver,VideoFrame
 from utils.mi_video_process import save_yuv_to_jpeg,yuv420_to_rgb
 import time
@@ -5,19 +7,41 @@ import os
 import subprocess
 import base64
 import logging
-import math
+
+class VideoH264Frame:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.buf_ = None
 
 logger = logging.getLogger(__name__)
-class MiVideoFrameObserver(IVideoFrameObserver):
+class MiZhiPuVideoFrameObserver(IVideoFrameObserver):
     def __init__(self):
-        super(MiVideoFrameObserver, self).__init__()
+        super(MiZhiPuVideoFrameObserver, self).__init__()
 
     def on_frame(self, video_frame_observer, channel_id, remote_uid, frame: VideoFrame):
         print("QiDebug, MiVideoFrameObserver , on_frame:", video_frame_observer, channel_id, remote_uid, frame.width,
               frame.height, frame.y_stride, frame.u_stride, frame.v_stride, len(frame.y_buffer), len(frame.u_buffer),
               len(frame.v_buffer))
 
-        print("QiDebug,MiVideoFrameObserver, on_frame, rotation: ",frame.rotation)
+        h264_frame = self.get_frame(frame)
+        return 0
+        # y_stride = 720;   u_stride=360; v_stride=360
+        # try:
+        #     if time.time() > self.event_handler.last_video_time + 0.3:
+        #         self.event_handler.last_video_time = time.time()
+        #         h264_frame = self.get_frame(frame)
+        #         asyncio.get_event_loop().run_until_complete(self.event_handler.video_cache.set([h264_frame]))
+        #         logger.info(f"[Python] on remote video cache: {remote_uid}")
+        #     return 1
+        # except Exception as e:
+        #     logger.error(f"[Python] on remote video frame error: {e}")
+        #     return 0
+
+    def get_frame(self, frame: VideoFrame) -> VideoH264Frame:
+        h264Frame = VideoH264Frame()
+        h264Frame.width = frame.width
+        h264Frame.height = frame.height
 
         # 创建用于存储YUV数据的缓冲区
         buffer = bytearray()
@@ -31,49 +55,42 @@ class MiVideoFrameObserver(IVideoFrameObserver):
         buffer.extend(frame.u_buffer[:uv_size])
         buffer.extend(frame.v_buffer[:uv_size])
 
+        h264Frame.buf_ = bytes(buffer)
+        logger.info(f"agora debug, get_frame buffer size: {len(buffer)} bytes")
+        logger.info(f"agora debug, get_frame width: {frame.width}, height: {frame.height}")
+        logger.info(
+            f"agora debug, get_frame y_buffer size: {len(frame.y_buffer)}, u_buffer: {len(frame.u_buffer)}, v_buffer: {len(frame.v_buffer)}")
+
         # 写入到当前目录的test.yuv文件中
-        file_path = os.path.join(os.getcwd(), 'test.yuv')  # 获取当前目录路径
+        file_path = os.path.join(os.getcwd(), 'test2.yuv')  # 获取当前目录路径
         with open(file_path, 'ab') as f:  # 'ab' 表示以二进制追加模式写入
             f.write(buffer)
 
         print(f"QiDebug, Buffer size: {len(buffer)} bytes")
 
         # 调用 extract_first_frame_to_base64，将 YUV 数据转换为 JPEG 的 Base64 编码
-        base64_jpeg = extract_first_frame_to_base64(buffer, frame.width, frame.height,frame.y_stride,frame.u_stride,frame.v_stride,frame.rotation)
+        base64_jpeg = extract_first_frame_to_base64(buffer, frame.width, frame.height)
 
         if base64_jpeg:
             print(f"QiDebug, Extracted frame as Base64 JPEG: {base64_jpeg[:100]}...")  # 打印前100个字符用于检查
         else:
             print("QiDebug, Failed to extract JPEG from YUV")
 
-        return 1
 
-def extract_first_frame_to_base64(yuv_bytes, width, height,yS,uS,vS,rotation):
+        return h264Frame
+
+def extract_first_frame_to_base64(yuv_bytes, width, height):
     # Prepare the FFmpeg command
-    # 如果旋转角度是 90, 180, 270
-    if rotation in [90, 180, 270]:
-        transpose_value = {
-            90: "transpose=1",  # 顺时针旋转 90 度
-            180: "transpose=2,transpose=2",  # 顺时针旋转 180 度 (两次90度旋转)
-            270: "transpose=2"  # 顺时针旋转 270 度
-        }[rotation]
-        vf_filter = f"crop={width}:{height},{transpose_value}"
-    else:
-        vf_filter = f"crop={width}:{height}"
-
     command = [
         "ffmpeg",
         "-f", "rawvideo",  # 输入是原始视频数据
         "-pix_fmt", "yuv420p",  # YUV420P 像素格式
-        "-s", f"{yS}x{height}",  # 视频的宽高
+        "-s", f"{width}x{height}",  # 视频的宽高
         "-i", "pipe:0",  # 从标准输入读取数据
-        "-vf", vf_filter,  # 旋转并裁剪
         "-vframes", "1",  # 只提取一帧
         "-f", "mjpeg",  # 输出为 MJPEG 格式
         "pipe:1",  # 将结果输出到标准输出
     ]
-
-    print("qidebug, ffmpeg command ,  ",command)
 
     # Run FFmpeg command
     process = subprocess.Popen(
@@ -100,3 +117,4 @@ def extract_first_frame_to_base64(yuv_bytes, width, height,yS,uS,vS,rotation):
     base64_encoded = base64.b64encode(out)
 
     return base64_encoded.decode("utf-8")
+
